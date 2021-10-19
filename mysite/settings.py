9c5cc8b089fb777d5f05abd0761027ab38e6104f
Oliver <ocben1@gmail.com>
2021-10-20 09:19:00 +1100

    packaged for reusability

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-59nux9dcmhhnl(0a(+22=1ywj53zw&+5z#&2$=n2#k9v(8g+ln'
-
+SECRET_KEY = [
+    'django-polls/.env.secret_key'
+]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
